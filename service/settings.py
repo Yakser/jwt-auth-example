3b6a0ebd1b4eda8bfa0407dc7b42bbcb47003593
mserygin <matvey@serygin.ru>
2024-04-20 14:56:47 +0300

    feat: update config for AUTH_COOKIE

--- service/settings.py
+++ service/settings.py
@@ -81,7 +81,7 @@ default_user_authentication_rule",
     "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
     "SLIDING_TOKEN_LIFETIME": timedelta(minutes=1),
     "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(minutes=5),
-    # 'AUTH_COOKIE': 'access_token',  # Cookie name. Enables cookies if value is set.
+    'AUTH_COOKIE': 'access_token',  # Cookie name. Enables cookies if value is set.
     'AUTH_COOKIE_DOMAIN': None,     # A string like "example.com", or None for standard domain cookie.
     'AUTH_COOKIE_SECURE': False,    # Whether the auth cookies should be secure (https:// only).
     'AUTH_COOKIE_HTTP_ONLY': True,  # Http only cookie flag.It's not fetch by javascript.
